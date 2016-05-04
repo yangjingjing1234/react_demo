38a8b79178b3cb82fba684c812f7cd253c7658ed
menglj <menglingbujie@gmail.com>
2016-05-04 18:21:47 +0800

    edit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # react_demo
+
+### 安装依赖
+    cd react_demo;
+    npm install //安装nodejs的express和react相关模块
+    npm run dataserver //启动数据服务
+    npm run webserver //启动web服务器
+
+模块安装完成之后浏览器访问
+[http://localhost:8000](http://localhost:8000)
--- server/webserver.js
+++ server/webserver.js
@@ -2,7 +2,7 @@
 var http = require("http");
 var express = require("express");
 var app = express();
-app.use(express.static("../react"));
+app.use(express.static("../react_demo"));
 var server = app.listen(8000,function(){
   console.log("Start web server....");
 });
